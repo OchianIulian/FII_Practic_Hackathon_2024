f7f6c51ad08e4dcb660a676dd129ea7ddb330f22
OchianIulian <ochian_a@yahoo.ro>
2024-04-27 15:22:37 +0300

    feat: Add Services and repositories for Picture, TextFile and Video

---
+++ Backend/oauth2-authentication-demo/src/main/java/com/example/oauth2authenticationdemo/repository/VideoRepository.java
@@ -0,0 +1,6 @@
+package com.example.oauth2authenticationdemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRepository extends JpaRepository<Long, Long> {
+}
---
+++ Backend/oauth2-authentication-demo/src/main/java/com/example/oauth2authenticationdemo/service/TextFileService.java
@@ -0,0 +1,7 @@
+package com.example.oauth2authenticationdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TextFileService {
+}
